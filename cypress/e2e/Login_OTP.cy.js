36cd24bf24a80543f2055acf0447e7674c220430
dehghan82 <108016220+dehghan82@users.noreply.github.com>
2024-08-17 16:16:28 +0330

    feat:added two more scenarios to password login

--- cypress/e2e/Login_OTP.cy.js
+++ cypress/e2e/Login_OTP.cy.js
@@ -1,5 +1,64 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+import {
+  urls,
+  users,
+  handleUncaughtException,
+  interceptUserExists,
+  firstProfile,
+  interceptProfile,
+  interceptOtpRequest,
+} from "./utils";
+
+describe("Login scenarios", () => {
+  beforeEach(() => {
+    const exist_status=true;
+    const pass_status=false;
+    cy.visit("/");
+    cy.get("a.login-btn").contains("ورود").click();
+    cy.url().should("contain", "/auth");
+    handleUncaughtException();
+    interceptUserExists(exist_status,pass_status); 
+    firstProfile();
+    interceptProfile();
+    interceptOtpRequest();
+  });
+
+  function performOtpLogin() {
+    cy.get('input[name="username"]').focus().clear().type(users.otp_phone_user.phoneNumber);
+    cy.get('button[type="submit"]').contains("تایید").click();
+
+    cy.wait("@userExistance");
+    cy.wait("@OTP_send");
+    cy.contains("h5", "ورود").should("exist");
+
+    cy.get('div.text-end.mb-3').contains("کد تایید ارسال‌شده را وارد کنید");
+    cy.get('input[name="otpPassword"]').type(users.otp_phone_user.otpCode);
+    interceptProfile();
+    cy.get('button[type="submit"]').contains("ورود").click();
+
+    cy.wait("@OTP_validate")
+    // .then((interception) => {
+    //   console.log("OTP validation response:", interception.response);
+    //   // You can verify the token if needed
+    //   expect(interception.response.body).to.have.property('token', 'mockToken123');
+    // });
+
+     cy.wait("@firstProfile");
+     //cy.wait("@profile");
+     //.then((interception) => {
+    //   console.log("Profile response:", interception.response);
+    //   // Ensure the profile request was successful
+    //   expect(interception.response.statusCode).to.equal(200);
+
+    // });
+
+    cy.url().should("include", "/market");
+  }
+
+  it("Should login using phone number with otp login", () => {
+    performOtpLogin();
+  });
+
+  // it("Should login using Email with otp login", () => {
+  //   performOtpLogin(data.verifiedEmail);
+  // });
+});
--- cypress/e2e/utils.js
+++ cypress/e2e/utils.js
@@ -6,9 +6,16 @@ export const users = {
   phoneUser: {
     phoneNumber: "09360257232",
     password: "Z82F84_r87",
-    full_name:"نرگس دهقان",
+    full_name: "نرگس دهقان",
+  },
+  otp_phone_user: {
+    verifiedEmail: "z.abbasgholi10@gmail.com",
+    phoneNumber: "09944854093",
+    otpCode: "123456",
   },
   username_user: {
+    email: "khalili.tayebeh55@gmail.com",
+    phoneNumber: "09360257232",
     username: "narges84",
     password: "Z82F84_r87",
     full_name: "نرگس دهقان",
@@ -31,16 +38,63 @@ export const urls = {
   index_daily_summary:
     "https://api-v2.tradebin.ir/market-overview/api/v1/index-daily-summary?page_size=-1",
   collect_data: "https://www.google-analytics.com/g/collect**",
+  otpVerify: "https://auth.tradebin.ir/auth/otp/verify/login-or-signup/",
 };
-export const interceptUserExists = () => {
+export const handleUncaughtException = () => {
+  Cypress.on("uncaught:exception", (err, runnable) => {
+    if (err.message.includes("Invalid token specified")) {
+      return false;
+    }
+    return true;
+  });
+};
+export const interceptUserExists = (exist_status, pass_status) => {
   cy.intercept("GET", urls.userExists, (req) => {
     req.continue((res) => {
-      expect(res.body).to.have.property("user_exists", true);
-      expect(res.body).to.have.property("user_has_password", true);
+      expect(res.body).to.have.property("user_exists", exist_status);
+      expect(res.body).to.have.property("user_has_password", pass_status);
     });
   }).as("userExistance");
 };
-
+export const firstProfile =() => {
+cy.intercept("GET","https://auth.tradebin.ir/auth/profile/?redirect_to=https://tradebin.ir&need_token=true",{
+    statusCode:200,
+}).as("firstProfile");
+};
 export const interceptProfile = () => {
-  cy.intercept("GET", urls.profile).as("profile");
+  cy.intercept("GET", urls.profile, {
+    statusCode: 200,
+    body: {
+      username: "zahra1212",
+      first_name: "زهرا",
+      last_name: "دهقان",
+      phone_number: "09944854093",
+      email: "farhangi.comp.iust@gmail.com",
+      email_verified: true,
+      birthday: "2003-03-03",
+      gender: "female",
+      user_has_password: false,
+    },
+  }).as("profile");
+};
+
+export const interceptOtpRequest = () => {
+  cy.intercept("POST", urls.otpRequest, (req) => {
+    req.continue((res) => {
+      expect(res.body).to.have.property(
+        "description",
+        "OTP sent successfully."
+      );
+    });
+  }).as("OTP_send");
+
+  cy.intercept("POST", urls.otpVerify, (req) => {
+    req.reply({
+      statusCode: 200,
+      body: {
+        success: true,
+        token: "mockToken123",
+      },
+    });
+  }).as("OTP_validate");
 };
